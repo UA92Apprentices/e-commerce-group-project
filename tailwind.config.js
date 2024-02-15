0056dbe5fbffc2a575f289bc06202efd9fd48d66
Hayden Buckley-Smith <148983130+HaydenBS93@users.noreply.github.com>
2024-02-15 10:58:15 +0000

    add custom theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        m: "375px",
+        t: "768px",
+        d: "1280px",
+      },
+    },
   },
   plugins: [],
-}
-
+};

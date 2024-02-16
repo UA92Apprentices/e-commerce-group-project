ed2f8177b1ed396aeda6d44090449e5caf8ecdf8
Hayden Buckley-Smith <148983130+HaydenBS93@users.noreply.github.com>
2024-02-16 10:01:16 +0000

    add Poppins font and set it as a custom token in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins"],
+      },
       screens: {
         m: "375px",
         t: "768px",
